251480d5aa60d37ebae9dfa0dbb8206ee7b99b43
casualchameleon <acasualchameleon@gmail.com>
2020-07-04 00:42:47 +0100

    fix scraper starting before yt is ready

--- modules/YouTubeApiScraperService.js
+++ modules/YouTubeApiScraperService.js
@@ -8,21 +8,15 @@ export class YouTubeApiScraperService {
 	async scrapeVideoNames(player) {
 		return new Promise(async (resolve, reject) => {
 
-			var videoMap = [];
-			for (let i=0; i < player.getPlaylist().length; i++) {
-				let data = player.getVideoData();
-				videoMap.push({
-					id: data.video_id,
-					title: data.title
-				});
+			let videoMap = [];
 
-				/*
-				* player.nextVideo() can time out sometimes, not sure why. So here, we put a timeout on it and retry up to 3 times, otherwise we throw.
+			 /*
+				* The player sometimes will do nothing (likely an API bug and onReady is being called too early) on the first track, not sure why. So here, we try three times to successfully get the right one before we start scraping. Otherwise we throw.
 				* This could probably be more elegant. The promise race works really nicely though.
-				*/
+			 */
 				for (let f = 0; f < 3; f++) {
 					try {
-						await this.getNextTrack(player);
+						await this.getTrack(player, 0);
 						break;
 					} catch(ex) {
 						MusicStreaming.log('getNextTrack timed out, retrying...');
@@ -31,13 +25,25 @@ export class YouTubeApiScraperService {
 						}
 					}
 				}
+
+			for (let i=0; i < player.getPlaylist().length; i++) {
+
+				await this.getTrack(player, i);
+
+				let data = player.getVideoData();
+				videoMap.push({
+					id: data.video_id,
+					title: data.title
+				});
+
+				
 			}
 
 			resolve(videoMap);
 		});
 	}
 	
-	async getNextTrack(player) {
+	async getTrack(player, idx) {
 		let playNextVideo = new Promise((resolve) => {
 			player.addEventListener('onStateChange', event => {
 				if (event.data == -1) {
@@ -45,7 +51,7 @@ export class YouTubeApiScraperService {
 				  resolve(event.data);
 				}
 			});
-			player.nextVideo();
+			player.playVideoAt(idx);
 		});
 
 		let timeout = new Promise((resolve, reject) => {
