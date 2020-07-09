6042aa840939b85c775828bb4e1031554d33390f
casualchameleon <acasualchameleon@gmail.com>
2020-07-09 23:22:13 +0100

    fix line indentation

--- modules/main.js
+++ modules/main.js
@@ -6,16 +6,16 @@ import './patches/index.js';
 Hooks.on("init", async () =>
 {
 	await initializeApis();
-
+	
 	await preloadTemplates();
 });
 
 Hooks.on("renderPlaylistDirectory", (app, html, data) => {
 	if (game.user.isGM) {
-			const importButton = $(`<button class="import-yt-playlist"><i class="fab fa-youtube"></i>${game.i18n.localize('music-assist.import-yt-playlist-nav-text')}</button>`);
-			html.find(".directory-footer").append(importButton);
-			importButton.click((ev) => {
-				new PlaylistImportForm().render(true);
+		const importButton = $(`<button class="import-yt-playlist"><i class="fab fa-youtube"></i>${game.i18n.localize('music-assist.import-yt-playlist-nav-text')}</button>`);
+		html.find(".directory-footer").append(importButton);
+		importButton.click((ev) => {
+			new PlaylistImportForm().render(true);
 		});	
 	}
 });
